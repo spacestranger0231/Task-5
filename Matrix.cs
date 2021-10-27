496a1996e28473fa88032771a8427566fc971e1d
Mukovoz T <91981870+spacestranger0231@users.noreply.github.com>
2021-10-27 21:06:00 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace таск_5
+{
+    class Matrix
+    {
+        int n, m, p;
+        const int size = 100;
+        int[,,] mat = new int[size, size, size];
+
+        public void Input()
+        {
+            Console.WriteLine("\nYour size (3 numbers): ");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            p = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < p; k++)
+                    {
+                        Random numbers = new Random();
+                        mat[i, j, k] = numbers.Next(0, 2);
+                    }
+                }
+            }
+        }
+
+        public void For_n()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < p; k++)
+                    {
+                        Console.Write("{0,2}", mat[i, 0, 0]);
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(' ');
+            }
+        }
+
+        public void For_m()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < p; k++)
+                    {
+                        Console.Write("{0,2}", mat[0, j, 0]);
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(' ');
+            }
+        }
+
+        public void For_p()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < p; k++)
+                    {
+                        Console.Write("{0,2}", mat[0, 0, k]);
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(' ');
+            }
+        }
+
+        public void Output_all()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < p; k++)
+                    {
+                        Console.Write("{0,2}", mat[i, j, k]);
+                    }
+
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(' ');
+            }
+        }
+        
+
+    }
+}
---
+++ Program-2 main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace таск_5
+{
+    class Program
+    {
+        static int Menu()
+        {
+            int a;
+            Console.WriteLine("Your choice:" + "\n1.Normal matrix" + "\n2.For n proection" + "\n3.For m proection" + "\n4.For p proection"
+                + "\n5.EXIT");
+            a = Convert.ToInt32(Console.ReadLine());
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix();
+            m.Input();
+            int b;
+            do
+            {
+                b = Menu();
+                switch (b)
+                {
+                    case 1:
+                        m.Output_all();
+                        break;
+                    case 2:
+                        m.For_n();
+                        break;
+                    case 3:
+                        m.For_m();
+                        break;
+                    case 4:
+                        m.For_p();
+                        break;
+                    case 5:
+                        Console.WriteLine("It was a pleasure to see you!");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        break;
+                }
+            } while (b != 5);
+        }
+    }
+}
