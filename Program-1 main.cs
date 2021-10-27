21c125482cca0f47aacbd0edac95a6fb3b013b1f
Mukovoz T <91981870+spacestranger0231@users.noreply.github.com>
2021-10-27 16:17:14 +0300

    Add files via upload

---
+++ Program-1 main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace таск_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\c#\таск 5\таск 5\Text.txt";
+            try
+            {
+                StreamReader f = new StreamReader(path);
+                Strings v = new Strings();
+                v.input(f);
+                v.output();
+                v.change();
+                Console.WriteLine("\n");
+                v.output();
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace таск_5
+{
+    class Strings
+    {
+        string[] arr = new string[35];
+        int kilkist;
+        public void input(StreamReader sw)
+        {
+            int i = 0;
+            while (sw.Peek() != -1)
+            {
+                arr[i] = sw.ReadLine();
+                i++;
+            }
+            kilkist = i;
+        }
+
+        public void output()
+        {
+            for(int i = 0; i < kilkist; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        public void change()
+        {
+            for (int i = 0; i < kilkist; i++)
+            {
+                arr[i] = arr[i].Replace('<', '#');
+            }
+
+            for (int i = 0; i < kilkist; i++)
+            {
+                arr[i] = arr[i].Replace('>', '#');
+            }
+        }
+    }
+}
+
